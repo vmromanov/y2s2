ce89c2aaeacb827ec4716482dc440895fd0ea3b5
vmromanov <145842293+vmromanov@users.noreply.github.com>
2025-04-07 15:02:10 +0300

    Add files via upload

---
+++ Lexical_Analyzer.h
@@ -0,0 +1,538 @@
+﻿#pragma once
+
+#include <vector>
+#include <fstream>
+
+#include "strTable.h"
+
+using namespace std;
+
+
+enum Lexeme_Class {lable=1,operand,arifmetic_operation,RELATION,NEXT,LET,FOR,GOTO,GOSUB,l_bracket,r_bracket,IF,RETURN,END,TO,STEP,REM,ERROR,ENDF};
+enum operation {plus_o=1,minus_o,mult_o,div_o,exponenciation_o};
+enum relation {EQ=1,LESS,GREATER,LEQ,GREQ,NOTEQ};
+
+enum simbolic_class{ letter,digit,arifmetic_op,relation,l_br,r_br,dot,space,endln,endf,errors};
+
+struct Lexeme
+{
+	Lexeme_Class LC;
+	int value;
+
+};
+
+
+class Lex_an
+{
+	simbolic_class translit(char ch);
+
+	void Error_handler(int N_str, string err_msg);
+
+	void create_lexeme();
+
+	string str; //строка входного файла
+
+	int RI;     //регистр номера буквы в имени переменной
+
+	int ROT;	//регистр отношения
+
+	int RSTR;	//регистр строки
+
+	bool RZ;    //регистр знака
+
+	int RCH;	//регистр числа
+
+	int RP;		//регистр порядка
+
+	int RS;		//регистр счетчика
+
+	int NTS;	//указатель на 1й свободный элемент в TS в обл переполнения
+
+	int NTO;	//указ на первый свобод элем в 
+
+	int NTL;	//указ на первый свобод элем в таблице лексем
+
+	int RKL;	//регистр класа лексем
+
+	int RZN;	//регистр значения символа
+
+	int RSOS;	//регистр состояния
+
+	int ROB;	//регистр обнаружения
+
+	int RK;		//регистр входного символа
+
+	int RSE;	//реристр счетчика ошибок
+
+	Lexeme* TL;	//указатель на начело таблицы лексем
+
+	double* TO; //указатель на начало таблицы операндов 
+//где то 500 эл
+	Hash* TS;	//указатель на начало таблицы строк
+
+
+
+	void(Lex_an::* q)();
+
+public:
+
+	Lex_an();
+
+	~Lex_an();
+
+	void print();
+
+	void start(string file_name); // построчно читаем
+
+	void A1();
+
+	void A2();
+
+	void A3();
+
+	void B1();
+
+	void C1();
+
+	void C2();
+
+	void D1();
+
+	void D2();
+
+	void D3();
+
+	void D4();
+
+	void D5();
+
+	void D6();
+
+	void E1();
+
+	void E2();
+
+	void F1();
+
+	void F2();
+
+	void F3();
+
+	void G1();
+
+	void H1();
+
+	void M1();
+
+	void M2();
+
+	void M3();
+
+	void Exit1();
+
+	void Exit2();
+
+	void Exit3();
+
+	void Exit4();
+
+	void Exit5();
+
+
+	void A1a() {
+		create_lexeme();
+		q = &Lex_an::A1;
+	}
+
+	void A1b() {
+		DA1D();
+		q = &Lex_an::A1;
+	}
+
+	void A1c() {
+		DA2D();
+		A1a();
+	}
+
+	void A1d() {
+		DA3D();
+		A1a();
+	}
+
+	void A1e() {
+		DA1E();
+		A1a();
+	}
+
+	void A2a() {
+		RKL = arifmetic_operation;
+		q = &Lex_an::A2;
+	}
+
+	void A2b() {
+		create_lexeme();
+		q = &Lex_an::A2;
+	}
+
+	void A2c() {
+		create_lexeme();
+		RKL = arifmetic_operation;
+		create_lexeme();
+		q = q = &Lex_an::A2;
+	}
+
+	void A2d() {
+		DA2D();
+		A2g();
+		q = &Lex_an::A2;
+	}
+
+	void A2e() {
+		DA3D();
+		q = &Lex_an::A2;
+	}
+
+	void A2f() {
+		DA1E();
+		q = &Lex_an::A2;
+	}
+
+	void A2g() {
+		create_lexeme();
+		RKL = arifmetic_operation;
+		create_lexeme();
+		q = &Lex_an::A2;
+	}
+
+	void A2h() {
+		RKL = l_bracket;
+		create_lexeme();
+		q = &Lex_an::A2;
+	}
+
+	void A2j() {
+		DA1E;
+		q = &Lex_an::A2;
+	}
+
+	void A2k() {
+		create_lexeme();
+		q = &Lex_an::A2;
+	}
+
+	void A2l() {
+		DA1D();
+		A2k();
+		q = &Lex_an::A2;
+	}
+
+	void A2m() {
+		DA2D();
+		A2k();
+		q = &Lex_an::A2;
+	}
+
+	void A2n() {
+		DA3D();
+		A2l();
+		q = &Lex_an::A2;
+	}
+
+	void A2o() {
+		if (RZN != 1)
+		{
+			G1b();
+			return;
+		}
+		create_lexeme();
+		q = &Lex_an::A2;
+	}
+
+	void A2p() { // нужна таблица с двойными операторами отношений
+		int r = 
+	}
+
+	void A2q() {
+		RKL = ENDF;
+		A2b();
+	}
+
+	void A2r() {
+		RKL = IF;
+		A2b();
+	}
+
+	void A2s() {
+		RKL = RETURN;
+		A2b();
+	}
+
+	void A2t() {
+		RKL = STEP;
+		A2b();
+	}
+
+	void A2u() {
+		RKL = Lexeme_Class::TO;
+		A2b();
+		q = &Lex_an::A2;
+	}
+
+	void A3a() {
+		RZN = (RZN + 1) * 26;
+		aaa
+	}
+
+	void A3b() {
+		RKL = r_bracket;
+		create_lexeme();
+		q = &Lex_an::A3;
+	}
+
+	void A3c()
+	{
+		create_lexeme();
+		q = &Lex_an::A3;
+	}
+
+	void A3d() {
+		DA1D();
+		q = &Lex_an::A3;
+	}
+
+	void A3e() {
+		DA2D();
+		A3c();
+		q = &Lex_an::A3;
+	}
+
+	void A3f() {
+		DA3D();
+		A3c();
+		q = &Lex_an::A3;
+	}
+
+	void A3g() {
+		DA1E();
+		A3c();
+		q = &Lex_an::A3;
+	}
+
+	void B1a() {
+		// нужн таблица обнаружения
+		⁘
+	}
+
+	void B1b() {
+		create_lexeme();
+		q = &Lex_an::B1;
+	}
+
+	void B1c() {
+		DA3D();
+		q = &Lex_an::B1;
+	}
+
+	void B1d() {
+		ROB++;
+		q = &Lex_an::B1;
+	}
+
+	void B1e() {
+		create_lexeme();
+		TL[NTL].LC = (Lexeme_Class)RKL;
+		int rs = TS->Find(RSTR);
+		if (rs==-1)
+		{
+			int insertion_pos = TS->el_add(RSTR, NTL);
+			TL[NTL].value = insertion_pos;
+			NTL++
+		}
+		q = &Lex_an::B1;
+	}
+
+	void C1a() {
+		RKL = END;// конец цикла
+		q = &Lex_an::C1;
+	}
+
+	void C2a() {
+		RKL = operand;
+		RI = RZN;
+		q = &Lex_an::C2;
+	}
+
+	void C2b() {
+		create_lexeme();
+		RKL = operand;
+		RI = RZN;
+		q = &Lex_an::C2;
+	}
+
+	void C2d() {
+		RI = RZN;
+	}
+
+	void D1a() {
+		RKL = operand;
+		RCH = RZN;
+		q = &Lex_an::D1;
+	}
+
+	void D1b() {
+		RCH *= 10;
+		RCH += RZN;
+		q = &Lex_an::D1;
+	}
+
+	void D1c() {
+		create_lexeme();
+		q = &Lex_an::D1;
+	}
+
+	void D2a() {
+		RS++;
+		RCH *= 10;
+		RCH += RZN;
+	}
+
+	void D2b() {
+		RS = 1;
+		RCH = RZN;
+		q = &Lex_an::D2;
+	}
+
+	void D2c() {
+		RS = 0;
+		q = &Lex_an::D2;
+	}
+
+	void D3a() {
+		RS = 0;
+		q = &Lex_an::D3;
+	}
+
+	void D4a() {
+		if (RZN == plus_o) { RZ = 1; q = &Lex_an::D4; }
+		if (RZN == minus_o) { RZ = 0; q = &Lex_an::D4; }
+		G1b();
+		
+	}
+
+	void D5a() {
+		RZ = 1;
+		q = &Lex_an::D5;
+	}
+
+	void D5b() {
+		RP = RZN;
+		q = &Lex_an::D5;
+	}
+
+	void D5c() {
+		RP *= 10;
+		RP += RZN;
+		q = &Lex_an::D5;
+	}
+
+	void D6a() {
+		create_lexeme();
+	}
+
+	void E1a() {
+		RKL = GOTO;
+		q = &Lex_an::E1;
+	}
+
+	void E1b() {
+		RKL = GOSUB;
+		q = q = &Lex_an::E1;
+	}
+
+	void E2a() {
+		RSTR=RZN;
+		q = &Lex_an::E2;
+	}
+
+	void E2b() {
+		RSTR = RZN;
+		q = &Lex_an::E2;
+	}
+
+	void E2c() {
+		RSTR *= 10;
+		RSTR += RZN;
+		q = &Lex_an::E2;
+	}
+
+	void F1a() {
+		RKL = LET;
+		q = &Lex_an::F1;
+	}
+
+	void F1b() {
+		RKL = FOR;
+		q = &Lex_an::F1;
+	}
+
+	void F2a() {
+		RI = RZN;
+		q = &Lex_an::F2;
+	}
+
+	void F3a() {
+		RI += RZN * 26;
+		q = &Lex_an::F3;
+	}
+
+	void G1a() {
+		RKL = REM;
+		create_lexeme();
+		q = &Lex_an::G1;
+	}
+
+	void G1b() {
+		RKL = ERROR;
+		create_lexeme();
+		q = &Lex_an::G1;
+	}
+
+	void H1a() {
+		RKL = RELATION;
+		ROT = RZN;
+		q = &Lex_an::H1;
+	}
+
+	void H1b() {
+		create_lexeme();
+		RKL = RELATION;
+		ROT = RZN;
+		q = &Lex_an::H1;
+	}
+
+	void H1c() {
+		create_lexeme();
+		RKL = RELATION;
+		ROT = RZN;
+		q = &Lex_an::H1;
+	}
+
+	void H1f() {
+		H1b();
+		RKL = RELATION;
+		ROT = RZN;
+		q = &Lex_an::H1;
+	}
+
+	void DA1D() {
+
+	}
+
+	void DA2D();
+
+	void DA3D();
+
+	void DA1E();
+
+	void DA1Ecicle();
+};
---
+++ strTable.h
@@ -0,0 +1,211 @@
+#pragma once
+
+#include <iostream>
+#include <iomanip>
+
+struct TS_el
+{
+	int label;		//значение 
+	int index_el;	//положение в таблице лексем
+	int next;		//разрешение колизий
+};
+
+class Hash
+{
+	int n_max;	
+	int n_os;	//размер основной области
+	int nts;	//первая свободная область в области переполнения
+
+	TS_el* t;
+
+	int hashfunc(int key) { return (key % n_os) + 1; }
+
+public:
+
+	Hash(int n1 = 1000, int os = 200)
+	{
+		n_max = n1;
+		n_os = os;
+		nts = os + 1;
+
+		t = new TS_el[n_max];
+
+		for (int i = 0; i < n_max; i++)
+		{
+			t[i].label = t[i].index_el = -1;
+			t[i].next = 0;
+		}
+	}
+
+	Hash(const Hash& other)
+	{
+		n_max = other.n_max;
+		n_os = other.n_os;
+		nts = other.nts;;
+		t = new TS_el[n_max];
+
+		for (int i = 0; i < n_max; i++)
+		{
+			t = other.t;
+		}
+	}
+
+	Hash(Hash& other)
+	{
+		n_max = other.n_max;
+		n_os = other.n_os;
+		nts = other.nts;;
+		t = new TS_el[n_max];
+
+		for (int i = 0; i < n_max; i++)
+		{
+			t = other.t;
+		}
+	}
+
+	~Hash()
+	{
+		delete[] t;
+	}
+
+	int Find(int key) // возвращяет номер элемента в таблице по ключу
+	{
+		int i = hashfunc(key);
+
+		while (t[i].label != key)
+		{
+			if (t[i].next != 0)
+				i = t[i].next;
+			else
+				return -1;
+		}
+		return i;
+	}
+
+	// добав элем, удал элем
+
+	void el_del(int key)
+	{
+
+
+		int k = hashfunc(key);
+		if (t[k].label == key && t[k].next == 0) // удаляем элемент в основной области у которого нет следующих
+		{
+			t[k].label = -1;
+			t[k].index_el = -1;
+			return;
+		}
+
+
+		if (t[k].label == key && t[k].next != 0) // удаляем элемент в основной области у которого есть следующие
+		{
+			int p = k;
+			int sv = t[k].next; // индекс начала цепочки не в осн обл
+			while (t[t[p].next].next != 0) // x1 x2 x3 x4   p - индекс предпоследнего(x3)
+			{
+				p = t[p].next;
+			}
+			t[k] = t[t[p].next]; // последний элем цепочки в осн. область
+			t[k].next = sv;
+			t[t[p].next].next = 0;
+			t[t[p].next].index_el = -1;
+			t[t[p].next].label = -1;
+			if (t[0].next == 0) t[0].next = t[p].next;  // если не удаляли до этого - просто удаляем и помещяем индекс окна в t[0].next
+			else
+			{// если до этого были окна которые еще остались - в next прошлого окнаставим индекс нашего элемента а в t[0].next индекс нашего элемента
+				int cpy = t[0].next;
+				t[0].next = t[p].next;
+				t[t[p].next].next = cpy;
+			}
+			t[p].next = 0;
+			
+			return;
+		}
+
+
+		// пока значение следующего не совпадёт
+		while (t[t[k].next].label != key)    // x1 x2....xn-1 xn xn+1   | xn - ищим, k - индекс xn-1
+			k=t[k].next;
+
+		int cpy = t[k].next; // cpy - индекс xn
+		t[k].next = t[cpy].next;   // некст у xn-1 = xn+1
+		t[cpy].label = -1;
+		// t[cpy].index_el = -1;
+		if (t[0].next == 0) { t[0].next = cpy; t[cpy].next = 0; }// если в next у t[0] пусто то окон нет, добавляем индекс удаленного элемента в t[0].next тк создали окно
+		else
+		{     // окна были => в t[0].next помещяем индекс удаляемого элемента cpy, а в next у удаляемого элемента помещяем индекс прошлого окна 
+			int cpy1 = t[0].next; //cpy1 - индекс последнего удал до нашего удаления
+			t[0].next = cpy;
+			t[cpy].next = cpy1;
+		}
+		return;
+	}
+
+	int el_add(int key,int ind)  // переделать чтоб оно возвращяло куда вставило
+	{
+		int k = hashfunc(key);
+		if (t[k].label == -1)
+		{
+			t[k].label = key;
+			t[k].index_el = ind;
+			return k;
+		}
+
+		int p = k;
+		while (t[p].next != 0) // идем до конца next
+			p = t[p].next; // p - индекс последнего
+
+		
+
+		if (t[0].next == 0 ) // окон нет просто добав по адресу nts
+		{
+			t[p].next = nts;
+			t[nts].label = key;
+			t[nts].index_el = ind;
+			nts++;
+			return nts;
+		}
+
+		if (t[0].next != 0)
+		{
+			t[p].next = t[0].next;
+			int sv = t[0].next;  // sv - индекс последнего удаленного
+			t[0].next = t[sv].next; // на "вершину стека" помещяем индекс предпоследнего удаленного
+			t[sv].label = key;
+			t[sv].index_el = ind;
+			t[sv].next = 0;
+			return sv;
+		}
+
+	}
+
+	void print()
+	{
+		std::cout << std::setw(7) << 0 << "|"
+			<< std::setw(10) << t[0].label << "|"
+			<< std::setw(10) << t[0].index_el << "|"
+			<< std::setw(10) << t[0].next << "|" << std::endl;
+		for (int i = 1; i < n_max; i++)
+		{
+			if (t[i].label!=-1)
+			std::cout << std::setw(7) << i << "|"
+				<< std::setw(10) << t[i].label << "|"
+				<< std::setw(10) << t[i].index_el << "|"
+				<< std::setw(10) << t[i].next << "|"<<std::endl ;
+		}
+		std::cout << std::endl;
+	}
+
+
+
+
+};
+
+
+
+
+
+
+
+
+
