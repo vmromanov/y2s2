dc64f517b589917fb64fad96b9c508220b54fbc0
vmromanov <145842293+vmromanov@users.noreply.github.com>
2025-05-04 21:10:13 +0300

    Add files via upload

--- auto_MP/MP_auto.cpp
+++ auto_MP/MP_auto.cpp
@@ -1,6 +1,6 @@
 #include <stack>
 #include <string>
-
+#include <iostream>
 using namespace std;
 
 void Replace_st(stack<char> *stack,string s)
@@ -18,7 +18,7 @@ bool is_allowed(string str)
 	st.push('D');
 	st.push('j');
 	st.push('x');
-
+	
 	int sz = str.size();
 	for (int i = 0; i <= sz;) // i==sz => str[i]==eof
 	{
@@ -32,11 +32,16 @@ bool is_allowed(string str)
 				case '1':
 					Replace_st(&st, "11x");
 					i++;
+					break;
 				case '0':
 					st.pop();
+					break;
+				default:
+					return false;
 				}
 			else
 				return false;
+			break;
 		case 'D':
 			if (i != sz)
 				switch (str[i])
@@ -44,11 +49,64 @@ bool is_allowed(string str)
 				case '1':
 					st.push('1');
 					i++;
-				case '0':
+					break;
+				default:
 					return false;
 				}
-			else return true;
+			else
+				return true;
+			break;
+		case '1':
+			if (i != sz)
+				switch (str[i])
+				{
+				case '1':
+					st.pop();
+					i++;
+					break;
+				case '0':
+					st.pop();
+					i++;
+					break;
+				default:
+					return false;
+				}
+			else
+				return false;
+			break;
+		case 'k':
+			if (i != sz)
+				switch (str[i])
+				{
+				case '1':
+					st.pop();
+					st.pop();
+					i++;
+					break;
+				default:
+					return false;
+				}
+			else 
+				return false;
+			break;
+		case 'j':
+			if (i != sz)
+				switch (str[i])
+				{
+				case '1':
+					st.push('k');
+					i++;
+					break;
+				default:
+					return false;
 
+				}
+			else 
+				return false;
+			break;
+		default:
+			return false;
+			break;
 		}
 	}
 
@@ -56,7 +114,12 @@ bool is_allowed(string str)
 
 }
 
-
+int main()
+{
+	string s;
+	s = "11111100000000000011111111";
+	cout << is_allowed(s);
+}
 
 
 
